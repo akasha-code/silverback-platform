c3932efeda0db8c21cf2b646a568d26b1f324ebe
Guido <guido.quadrini@gmail.com>
2025-04-19 21:57:30 +0200

    feat: auth microfrontend federated into shell 🧬🦍

--- frontend/auth/vite.config.js
+++ frontend/auth/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'auth',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './AuthApp': './src/AuthApp.jsx',
+      },
+      shared: ['react', 'react-dom']
+    })
+  ],
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false
+  },
+  server: {
+    port: 3001,
+  }
+});
+
